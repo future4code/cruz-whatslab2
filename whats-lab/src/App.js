5e0c532bddfd9bd3832de3dc427fb5346541ac38
Marcelino Sandroni <marcelino.sandroni@gmail.com>
2021-03-12 12:00:30 -0300

    update

--- whats-lab/src/App.js
+++ whats-lab/src/App.js
@@ -1,15 +1,88 @@
 import './App.css';
 import React from 'react'
 import styled from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
 
-const container = styled.div`
+const GlobalStyle = createGlobalStyle`
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  list-style: none;
+`
+
+const Container = styled.div`
+  width: 80%;
+  height: 80vh;
+  margin: 20px auto;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: stretch;
+
+  border: 2px solid #333;
+
+`
+
+const ListaMensagem = styled.div`
+  flex-basis: 90%;
+
+  strong {
+    
+  }
+
+  span {
+    
+  }
+
+`
+
+const CriarMensagem = styled.div`
+  flex-basis: 10%;
+  height: 10%;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  
+
+  > * {
+    height: 93%;
+    font-size: 20px;
+  }
+
+  input {
+    margin: 0;
+    padding: 0;
+    
+  }
+
+  input::placeholder {
+    text-align: center;
+  }
+
+  input:first-of-type {
+
+    
+  }
+
+  input:last-of-type {
+    flex-grow: 1;
+    
+  }
+
+  button {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    width: 100px;
+
+  }
+
 `
 
 class App extends React.Component {
 
   state = {
-    mensagens: [{pessoa: "Marcelo", conteudo: "Olá"}, 
-    {pessoa:"Renata", conteudo: "Cheguei"}],
+    mensagens: [],
     inputUsuario: "",
     inputMensagem: ""
   }
@@ -37,17 +110,19 @@ class App extends React.Component {
       })
 
       return (
-      <div>
-        <section>
+      <Container>
+        <GlobalStyle/>
+        <ListaMensagem>
         {mensagens}
-        </section>
-        <section>
+        </ListaMensagem>
+        <CriarMensagem>
           <input type="text" placeholder="Usuário" onChange={this.atualizarUsuario} value={this.state.inputUsuario}/>
           <input type="text" placeholder="Mensagem" onChange={this.atualizarMensagem} value={this.state.inputMensagem}/>
           <button onClick={this.criarMensagem}>Enviar</button>
-        </section>
+        </CriarMensagem>
+        
 
-      </div>
+      </Container>
 
     );
   }
